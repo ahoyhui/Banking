d7a46645c32028a5f444f5d99c7afd157bf423f8
Keith Hui <ahoyhui123@gmail.com>
2018-12-04 17:55:30 +0000

    initial commit

---
+++ src/main/java/com/akz/bankingapi/Account.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.akz.bankingapi;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author akz
+ */
+@Entity
+@Table
+@XmlRootElement
+public class Account implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int user;
+
+    /**
+     * @return the id
+     */
+    public int getCustomer() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getUser() {
+        return user;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(int user) {
+        this.user = user;
+    }
+    
+
+   
+}
+
+    
+    
